e524b70871473102e4e43e5df245ef01441a52ea
srozemuller <43162899+srozemuller@users.noreply.github.com>
2020-12-04 11:06:29 +0100

    Update deploy-laws.ps1

--- Azure Monitor/deploy-laws.ps1
+++ Azure Monitor/deploy-laws.ps1
@@ -1,13 +1,40 @@
+param(
+    [parameter(mandatory = $false)][string]$EventsTemplate,
+    [parameter(mandatory = $false)][string]$CountersTemplate,
+    [parameter(mandatory = $true)][string]$ResourceGroup,
+    [parameter(mandatory = $true)][string]$Location,
+    [parameter(mandatory = $false)][string]$WorkspaceName
+
+)
 Import-Module Az.OperationalInsights
 
-$ResourceGroup = "[rg]"
-$WorkspaceName = "log-analytics-wvd-" + (Get-Random -Maximum 99999) # workspace names need to be unique across all Azure subscriptions - Get-Random helps with this for the example code
-$Location = "westeurope"
+if ($null -eq $WorkspaceName){
+Write-Host "No Log Analytics Workspace name provided, creating new Workspace"
+$WorkspaceName = "log-analytics-wvd-"+ (Get-Random -Maximum 99999) # workspace names need to be unique across all Azure subscriptions - Get-Random helps with this for the example code
+}
+Write-Host "Created workspace $WorkspaceName"
+
+$WindowsEvents = Get-Content $EventsTemplate | ConvertFrom-Json
+$PerformanceCounters = Get-Content $CountersTemplate | ConvertFrom-Json
+
+function Get-CorrectEventLevels($EventLevels) {
+    $CollectInformation, $collectWarnings, $collectErrors = $false
+    if (($WindowsEventLog.EventTypes).Contains("Information")) { $CollectInformation = $true }
+    if (($WindowsEventLog.EventTypes).Contains("Warning")) { $collectWarnings = $true }
+    if (($WindowsEventLog.EventTypes).Contains("Error")) { $collectErrors = $true }
+    $eventLevels = @{
+        collectInformation = $CollectInformation
+        collectWarnings    = $collectWarnings
+        collectErrors      = $collectErrors
+    }
+    $eventLevels
+}
 
 # Create the resource group if needed
 try {
     Get-AzResourceGroup -Name $ResourceGroup -ErrorAction Stop
-} catch {
+}
+catch {
     New-AzResourceGroup -Name $ResourceGroup -Location $Location
 }
 
@@ -17,15 +44,39 @@ New-AzOperationalInsightsWorkspace -Location $Location -Name $WorkspaceName -Sku
 # Create the resource group if needed
 try {
     Get-AzResourceGroup -Name $ResourceGroup -ErrorAction Stop
-} catch {
+}
+catch {
     New-AzResourceGroup -Name $ResourceGroup -Location $Location
 }
 
 # Create the workspace
 New-AzOperationalInsightsWorkspace -Location $Location -Name $WorkspaceName -Sku Standard -ResourceGroupName $ResourceGroup
 
-# Windows Event
-New-AzOperationalInsightsWindowsEventDataSource -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName -EventLogName "System" -CollectErrors -CollectWarnings -Name "System warning and error Event Log"
+If ($EventsTemplate) {
+    foreach ($WindowsEventLog in $WindowsEvents.WindowsEvent.EventLogNames) {
+        $Level = Get-CorrectEventLevels -EventLevels $WindowsEventLog.EventTypes
+        if (($WindowsEventLog.Value).Contains("/") ) { $name = $WindowsEventLog.Value.Replace("/", "-") }
+        else { $name = $WindowsEventLog.Value }
+        # Windows Event
+        New-AzOperationalInsightsWindowsEventDataSource -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName -EventLogName $WindowsEventLog.Value -Name $name @Level -Confirm:$false
+    }
+}
 
-# Windows Perf
-New-AzOperationalInsightsWindowsPerformanceCounterDataSource -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName -ObjectName "LogicalDisk" -InstanceName "C:" -CounterName "% Free Space" -IntervalSeconds 60 -Name "C percent free space"
+If ($CountersTemplate) {
+    foreach ($CounterObject in $PerformanceCounters.WindowsPerformanceCounter) {
+        $CounterObject
+        foreach ($Counter in $CounterObject.Counters) {
+            if (($Counter.name).Contains("/") ) { $name =$Counter.name.Replace("/", "-") }
+            else { $name = $Counter.name }
+            $Parameters = @{
+                ObjectName      = $CounterObject.Object
+                InstanceName    = $Counter.InstanceName
+                CounterName     = $Counter.CounterName
+                IntervalSeconds = $Counter.IntervalSeconds
+                Name            = $Name
+            }
+            $Parameters
+            New-AzOperationalInsightsWindowsPerformanceCounterDataSource -ResourceGroupName $ResourceGroup -WorkspaceName $WorkspaceName @parameters
+        }
+    }
+}

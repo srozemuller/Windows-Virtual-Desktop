34733d8249c57c249bec2be5cc2c9d1eb7ff10ff
srozemuller@uniccloud.nl <srozemuller@uniccloud.nl>
2021-03-04 08:38:06 +0100

    initial

---
+++ PowerShell-Modules/AzWvd/Public/Get-WvdSessionHostResources.ps1
@@ -0,0 +1,26 @@
+#requires -module @{ModuleName = 'Az.Resources'; ModuleVersion = '3.2.1'}
+
+function Get-WvdSessionHostResources {
+    <#
+    .SYNOPSIS
+    Gets the Virtual Machines Azure resource from a WVD Session Host
+    .DESCRIPTION
+    The function will help you getting the virtual machine resource information which is behind the WVD Session Host
+    .PARAMETER SessionHost
+    Enter the WVD Session Host name
+    .EXAMPLE
+    Get-WvdSessionHostResources -SessionHost SessionHostObject
+    Add a comment to existing incidnet
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(mandatory = $true, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [Object]$SessionHost
+    )
+    
+    $Resource = Get-AzResource -ResourceId $SessionHost.ResourceId
+    $VirtualMachine = (Get-AzVm -Name $Resource.Name)
+    Write-Verbose "Found virtual machine: $($Resource.Name)"
+    return $VirtualMachine
+}

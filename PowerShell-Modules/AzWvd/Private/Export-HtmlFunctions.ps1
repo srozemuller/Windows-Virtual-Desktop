1b65ae7cd4fc98d1d50d8f394694281f7a8cee3a
srozemuller@uniccloud.nl <srozemuller@uniccloud.nl>
2021-03-08 12:31:10 +0100

    some formatting css

--- PowerShell-Modules/AzWvd/Private/Export-HtmlFunctions.ps1
+++ PowerShell-Modules/AzWvd/Private/Export-HtmlFunctions.ps1
@@ -23,14 +23,57 @@ function Get-SessionHostHtmlContent {
         [ValidateNotNullOrEmpty()]
         [string]$ResourceGroupName
     )
+    $SessionHostContent = Get-WvdImageVersionStatus @SessionHostParameters 
     $ConvertParameters = @{
-        Property = "Name"
+        Property = "Name","AgentVersion","AllowNewSession","OSVersion","HasLatestVersion","CurrentVersion"
         Fragment = $true
+        PreContent = "<p>Sessionhosts in $HostpoolName</p>"
     }
     $SessionHostParameters = @{
         HostPoolName      = $HostpoolName 
-        ResourceGroupName = $ResourceGroup 
+        ResourceGroupName = $ResourceGroupName 
     }
-    $SessionHostContent = Get-AzWvdSessionHost @SessionHostParameters | ConvertTo-Html @ConvertParameters
+    $SessionHostContent = Get-WvdImageVersionStatus @SessionHostParameters | ConvertTo-Html @ConvertParameters
+    $SessionHostContent = $SessionHostContent -replace '<td>True</td>','<td class="GoodStatus">True</td>'
+    $SessionHostContent = $SessionHostContent -replace '<td>False</td>','<td class="WrongStatus">False</td>'
     return $SessionHostContent
 }
+
+function Get-SubnetConfigHtmlContent {
+    <#
+    .SYNOPSIS
+    Exports the complete Windows Virtual Desktop environment, based on the hostpool name.
+    .DESCRIPTION
+    The function will help you exporting the complete WVD environment to common output types as HTML and CSV.
+    .PARAMETER HostpoolName
+    Enter the WVD hostpoolname name.
+    .PARAMETER ResourceGroupName
+    Enter the WVD hostpool resource group name.
+    .PARAMETER 
+    .EXAMPLE
+    Export-WvdConfig -Hostpoolname $hostpoolName -resourceGroup $ResourceGroup -Scope Hostpool,SessionHosts -Verbose -FilePath .\wvdexport.html
+    Add a comment to existing incidnet
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ParameterSetName = 'Parameters')]
+        [ValidateNotNullOrEmpty()]
+        [string]$HostpoolName,
+
+        [parameter(Mandatory, ParameterSetName = 'Parameters')]
+        [ValidateNotNullOrEmpty()]
+        [string]$ResourceGroupName
+    )
+    $ConvertParameters = @{
+        Property = "Name","PrivateEndpointNetworkPolicies","PrivateLinkServiceNetworkPolicies","NatGateway"
+        Fragment = $true
+        PreContent = "<p>SubnetConfig in $HostpoolName</p>"
+    }
+    $Parameters = @{
+        HostPoolName      = $HostpoolName 
+        ResourceGroupName = $ResourceGroupName 
+    }
+    $SubnetConfigContent =  Get-WvdSubnet @Parameters |  ConvertTo-Html @ConvertParameters
+    return $SubnetConfigContent
+}
+
